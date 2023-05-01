2bf6e0a07a47124799e932ed5c0ee7b0a560ad68
yelsabawyy <yelsabawyy@gmail.com>
2023-05-01 09:50:02 +0300

    first commit

---
+++ javascript/header.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "0%";
+    console.log("hello");
+  }
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0%";
+    document.getElementById("main").style.marginLeft= "100%";
+  }
+
+
